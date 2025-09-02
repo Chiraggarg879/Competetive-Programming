5d47250beff88580a787ef2d3b63849af9be1b88
Chirag Garg <chiraggarg879@gmail.com>
2025-09-02 22:36:29 +0530

    Added solutions of contest div 2

---
+++ C_Even_Larger.cpp
@@ -0,0 +1,38 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr);
+
+// Updated to use long long
+void solve(int n, vector<ll> &arr) {
+    ll res = 0;
+    for (int i = 1; i < n; i += 2) {
+        ll sodd = arr[i - 1];
+        if (i < n - 1) sodd += arr[i + 1];
+        ll diff = sodd - arr[i];
+        if (diff > 0) {
+            res += diff;
+        }
+        if (i < n - 1 && diff >0) arr[i + 1] -= diff;
+        if (i < n - 1 && arr[i + 1] < 0) arr[i + 1] = 0;
+    }
+    cout << res << "\n";
+}
+
+int main() {
+    fastio;
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<ll> arr(n);
+        for (int i = 0; i < n; i++) cin >> arr[i];
+        solve(n, arr);
+    }
+    return 0;
+}
