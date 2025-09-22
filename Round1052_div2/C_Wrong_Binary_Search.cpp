61d6c5ccde9c519791673f75f09f6f6482f89be4
Chirag Garg <chiraggarg879@gmail.com>
2025-09-22 21:27:49 +0530

    Corrected solution of wrong_binary_search

--- Round1052_div2/C_Wrong_Binary_Search.cpp
+++ Round1052_div2/C_Wrong_Binary_Search.cpp
@@ -4,61 +4,71 @@ using namespace std;
 #define ll long long
 #define pb push_back
 #define all(x) (x).begin(), (x).end()
-#define fastio ios::sync_with_stdio(false); cin.tie(nullptr);
-//wrong solution 
-void solve() {
-     // Your solution here
-     int n;
-     cin>>n;
-     string s;
-     cin>>s;
-     vector<int> res(n);
-     for(int i=0;i<n;i++){
-        res[i] = i+1;
-     }
-     int flag = 0;
-     int first = -1,second = -1;
-     for(int i=0;i<n;i++){
-        if(s[i] == '1'){
-            if(first != -1 && second !=-1){
-                if((second - first)%2!=0){
-                    flag = !flag;
-                    break;
-                }else{
-                    for(int i=first;i<=second;i+=2){
-                        swap(res[i],res[i+1]);
-                    }
-                }
-            }
-            first = -1;
-            second = -1;
-            continue;
-        }
-        else{
-            if(first == -1){
+#define fastio                   \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr);
+// wrong solution
+void solve()
+{
+    // Your solution here
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    vector<int> res(n);
+    for (int i = 0; i < n; i++)
+    {
+        res[i] = i + 1;
+    }
+    int flag = 0;
+    int first = -1, second = -1;
+    for (int i = 0; i < n; i++)
+    {
+
+        while (i < n && s[i] == '0')
+        {
+            if (first == -1)
+            {
                 first = i;
                 second = i;
             }
-            else{
+            else
+            {
                 second = i;
             }
+            i++;
         }
-     }
-     if(flag){
-        cout<<"NO"<<endl;
-     }else{
-        cout<<"YES"<<endl;
-        for(auto x:res) cout<<x<<" ";
-        cout<<endl;
-     }
 
+        if (first != -1 && second != -1)
+        {
+            if (first == second)
+            {
+                cout << "NO\n";
+                return;
+            }
+            else
+            {
+                swap(res[first], res[second]);
+                i--;
+            }
+        }
+        first = -1;
+    second = -1;
+    }
+
+    cout << "YES\n";
+    for (auto x : res)
+        cout << x << " ";
+    cout << endl;
 }
 
-int main() {
+int main()
+{
     fastio;
     int t = 1;
     cin >> t;
-    while (t--) {
+    while (t--)
+    {
         solve();
     }
     return 0;
